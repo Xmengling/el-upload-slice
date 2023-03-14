43adedcb93128f7210fb5750a1a5fd876890913a
c28381 <cheng.mengling@h3c.com>
2023-03-14 17:43:23 +0800

    feature: 项目初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <el-button>组件按钮</el-button>
+    <el-button @click="apiTestApi">api请求</el-button>
+  </div>
+</template>
+
+<script>
+import { apiTest } from '@/api/hello-world'
+export default {
+  name: 'HelloWorld',
+  components: {
+
+  },
+  methods: {
+    async apiTestApi() {
+      try {
+        await apiTest()
+      } catch (error) {
+        throw new Error()
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
